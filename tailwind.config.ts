0af6b8f6d1d912221072192508dd77c9984fff3a
Laporan-cek-aset <sukrozone@gmail.com>
2026-09-25 16:26:14 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,18 +2,12 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        kkgmiGreen: "#116530",
-        kkgmiGold: "#D4AF37",
-        kkgmiLightGreen: "#E8F5E9"
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
