8b0c16a214c6676d2a08622ad803080fc66c11cd
Laporan-cek-aset <sukrozone@gmail.com>
2026-09-26 08:38:13 +0700

    Update route.ts

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getTursoClient } from '../../../lib/turso';
 
-// PERINTAH WAJIB UNTUK VERCEL: Jangan cache API ini!
 export const dynamic = 'force-dynamic';
 
 export async function POST(request: Request) {
@@ -9,12 +8,9 @@ export async function POST(request: Request) {
     const body = await request.json();
     const { action, username, password, nama } = body; 
     
-    // Panggil koneksi database
     const client = getTursoClient();
 
-    // 1. LOGIKA REGISTER (Pendaftaran)
     if (action === 'register') {
-      // Cek apakah username sudah ada
       const checkUser = await client.execute({
         sql: "SELECT * FROM Users WHERE Username = ?",
         args: [username]
@@ -24,16 +20,13 @@ export async function POST(request: Request) {
         return NextResponse.json({ success: false, message: 'Username sudah digunakan.' }, { status: 400 });
       }
 
-      // Generate ID unik
       const newUserId = `U${Date.now()}`;
 
-      // Simpan Guru ke tabel Users (SQL Murni)
       await client.execute({
         sql: "INSERT INTO Users (ID, Role, Username, Password, Nama) VALUES (?, ?, ?, ?, ?)",
         args: [newUserId, "guru", username, password, nama]
       });
 
-      // Simpan tagihan otomatis ke tabel payments (SQL Murni)
       await client.execute({
         sql: "INSERT INTO payments (guru_id, amount, status) VALUES (?, ?, ?)",
         args: [newUserId, 0, "pending"]
@@ -42,14 +35,11 @@ export async function POST(request: Request) {
       return NextResponse.json({ success: true, message: 'Pendaftaran berhasil!' });
     }
 
-    // 2. LOGIKA LOGIN
     if (action === 'login') {
-      // Akses khusus Admin
       if (username === 'admin' && password === 'admin123') {
         return NextResponse.json({ success: true, role: 'admin', nama: 'Administrator' });
       }
 
-      // Cek data di Turso
       const checkUser = await client.execute({
         sql: "SELECT * FROM Users WHERE Username = ?",
         args: [username]
